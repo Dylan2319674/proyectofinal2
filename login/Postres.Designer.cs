a6ce5e8a7404159fb9d42bf98ed12234402656c6
SantiagoReyes2819 <regs090628hgtynna8@cbtis217.edu.mx>
2025-12-15 15:18:19 -0600

    Actualización del formulario Postres: totales, borrar y eliminar producto

--- login/Postres.Designer.cs
+++ login/Postres.Designer.cs
@@ -15,31 +15,419 @@ namespace login
 
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Postres));
 			this.btnRegresar = new System.Windows.Forms.Button();
+			this.TxtBorrar = new System.Windows.Forms.Button();
+			this.BtnGuardar = new System.Windows.Forms.Button();
+			this.TxtCambio = new System.Windows.Forms.TextBox();
+			this.LblCambio = new System.Windows.Forms.Label();
+			this.TxtImportePagado = new System.Windows.Forms.TextBox();
+			this.TxtImporteNeto = new System.Windows.Forms.TextBox();
+			this.LblImportePagado = new System.Windows.Forms.Label();
+			this.LblImporteNeto = new System.Windows.Forms.Label();
+			this.columnHeader5 = new System.Windows.Forms.ColumnHeader();
+			this.TxtDescuento = new System.Windows.Forms.TextBox();
+			this.LblDescuento = new System.Windows.Forms.Label();
+			this.TxtSubtotal = new System.Windows.Forms.TextBox();
+			this.LblSubtotal = new System.Windows.Forms.Label();
+			this.BtnEliminarProducto = new System.Windows.Forms.Button();
+			this.BtnAgregarProducto = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.BtnImprimir = new System.Windows.Forms.Button();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+			this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+			this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+			this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+			this.TxtTotal = new System.Windows.Forms.TextBox();
+			this.TxtCantidad = new System.Windows.Forms.TextBox();
+			this.TxtPrecio = new System.Windows.Forms.TextBox();
+			this.LblCantidad = new System.Windows.Forms.Label();
+			this.LblTotal = new System.Windows.Forms.Label();
+			this.LblPrecio = new System.Windows.Forms.Label();
+			this.cmbProducto = new System.Windows.Forms.ComboBox();
+			this.LblProducto = new System.Windows.Forms.Label();
+			this.RbdSalado = new System.Windows.Forms.RadioButton();
+			this.RbdDulce = new System.Windows.Forms.RadioButton();
+			this.BtnLimpiar = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// btnRegresar
 			// 
-			this.btnRegresar.Location = new System.Drawing.Point(118, 71);
+			this.btnRegresar.Location = new System.Drawing.Point(975, 779);
 			this.btnRegresar.Name = "btnRegresar";
 			this.btnRegresar.Size = new System.Drawing.Size(100, 70);
 			this.btnRegresar.TabIndex = 0;
 			this.btnRegresar.Text = "Calcular";
 			this.btnRegresar.UseVisualStyleBackColor = true;
-			this.btnRegresar.Click += new System.EventHandler(this.BtnRegresarClick);
+			// 
+			// TxtBorrar
+			// 
+			this.TxtBorrar.Location = new System.Drawing.Point(908, 364);
+			this.TxtBorrar.Name = "TxtBorrar";
+			this.TxtBorrar.Size = new System.Drawing.Size(96, 41);
+			this.TxtBorrar.TabIndex = 52;
+			this.TxtBorrar.Text = "Borrar";
+			this.TxtBorrar.UseVisualStyleBackColor = true;
+			// 
+			// BtnGuardar
+			// 
+			this.BtnGuardar.Location = new System.Drawing.Point(908, 220);
+			this.BtnGuardar.Name = "BtnGuardar";
+			this.BtnGuardar.Size = new System.Drawing.Size(96, 41);
+			this.BtnGuardar.TabIndex = 51;
+			this.BtnGuardar.Text = "Guardar";
+			this.BtnGuardar.UseVisualStyleBackColor = true;
+			// 
+			// TxtCambio
+			// 
+			this.TxtCambio.Location = new System.Drawing.Point(786, 727);
+			this.TxtCambio.Name = "TxtCambio";
+			this.TxtCambio.Size = new System.Drawing.Size(100, 22);
+			this.TxtCambio.TabIndex = 50;
+			// 
+			// LblCambio
+			// 
+			this.LblCambio.Location = new System.Drawing.Point(687, 730);
+			this.LblCambio.Name = "LblCambio";
+			this.LblCambio.Size = new System.Drawing.Size(73, 23);
+			this.LblCambio.TabIndex = 49;
+			this.LblCambio.Text = "Cambio";
+			// 
+			// TxtImportePagado
+			// 
+			this.TxtImportePagado.Location = new System.Drawing.Point(535, 776);
+			this.TxtImportePagado.Name = "TxtImportePagado";
+			this.TxtImportePagado.Size = new System.Drawing.Size(100, 22);
+			this.TxtImportePagado.TabIndex = 48;
+			// 
+			// TxtImporteNeto
+			// 
+			this.TxtImporteNeto.Location = new System.Drawing.Point(535, 731);
+			this.TxtImporteNeto.Name = "TxtImporteNeto";
+			this.TxtImporteNeto.Size = new System.Drawing.Size(100, 22);
+			this.TxtImporteNeto.TabIndex = 47;
+			// 
+			// LblImportePagado
+			// 
+			this.LblImportePagado.Location = new System.Drawing.Point(394, 779);
+			this.LblImportePagado.Name = "LblImportePagado";
+			this.LblImportePagado.Size = new System.Drawing.Size(112, 23);
+			this.LblImportePagado.TabIndex = 46;
+			this.LblImportePagado.Text = "Importe Pagado";
+			// 
+			// LblImporteNeto
+			// 
+			this.LblImporteNeto.Location = new System.Drawing.Point(394, 730);
+			this.LblImporteNeto.Name = "LblImporteNeto";
+			this.LblImporteNeto.Size = new System.Drawing.Size(100, 23);
+			this.LblImporteNeto.TabIndex = 45;
+			this.LblImporteNeto.Text = "Importe Neto";
+			// 
+			// columnHeader5
+			// 
+			this.columnHeader5.Text = "Sal o Dul";
+			this.columnHeader5.Width = 94;
+			// 
+			// TxtDescuento
+			// 
+			this.TxtDescuento.Location = new System.Drawing.Point(217, 780);
+			this.TxtDescuento.Name = "TxtDescuento";
+			this.TxtDescuento.Size = new System.Drawing.Size(100, 22);
+			this.TxtDescuento.TabIndex = 44;
+			// 
+			// LblDescuento
+			// 
+			this.LblDescuento.Location = new System.Drawing.Point(111, 779);
+			this.LblDescuento.Name = "LblDescuento";
+			this.LblDescuento.Size = new System.Drawing.Size(77, 23);
+			this.LblDescuento.TabIndex = 43;
+			this.LblDescuento.Text = "Descuento";
+			// 
+			// TxtSubtotal
+			// 
+			this.TxtSubtotal.Location = new System.Drawing.Point(217, 734);
+			this.TxtSubtotal.Name = "TxtSubtotal";
+			this.TxtSubtotal.Size = new System.Drawing.Size(100, 22);
+			this.TxtSubtotal.TabIndex = 42;
+			// 
+			// LblSubtotal
+			// 
+			this.LblSubtotal.Location = new System.Drawing.Point(111, 733);
+			this.LblSubtotal.Name = "LblSubtotal";
+			this.LblSubtotal.Size = new System.Drawing.Size(100, 23);
+			this.LblSubtotal.TabIndex = 41;
+			this.LblSubtotal.Text = "Subtotal";
+			// 
+			// BtnEliminarProducto
+			// 
+			this.BtnEliminarProducto.Location = new System.Drawing.Point(587, 376);
+			this.BtnEliminarProducto.Name = "BtnEliminarProducto";
+			this.BtnEliminarProducto.Size = new System.Drawing.Size(223, 37);
+			this.BtnEliminarProducto.TabIndex = 40;
+			this.BtnEliminarProducto.Text = "Eliminar Producto";
+			this.BtnEliminarProducto.UseVisualStyleBackColor = true;
+			this.BtnEliminarProducto.Click += new System.EventHandler(this.BtnEliminarProductoClick);
+			// 
+			// BtnAgregarProducto
+			// 
+			this.BtnAgregarProducto.Location = new System.Drawing.Point(587, 316);
+			this.BtnAgregarProducto.Name = "BtnAgregarProducto";
+			this.BtnAgregarProducto.Size = new System.Drawing.Size(223, 37);
+			this.BtnAgregarProducto.TabIndex = 39;
+			this.BtnAgregarProducto.Text = "Agragar Producto";
+			this.BtnAgregarProducto.UseVisualStyleBackColor = true;
+			this.BtnAgregarProducto.Click += new System.EventHandler(this.BtnAgregarProductoClick);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(27, 19);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(1026, 182);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 27;
+			this.pictureBox1.TabStop = false;
+			// 
+			// BtnImprimir
+			// 
+			this.BtnImprimir.Location = new System.Drawing.Point(908, 296);
+			this.BtnImprimir.Name = "BtnImprimir";
+			this.BtnImprimir.Size = new System.Drawing.Size(96, 41);
+			this.BtnImprimir.TabIndex = 53;
+			this.BtnImprimir.Text = "Imprimir";
+			this.BtnImprimir.UseVisualStyleBackColor = true;
+			// 
+			// listView1
+			// 
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+									this.columnHeader1,
+									this.columnHeader2,
+									this.columnHeader3,
+									this.columnHeader4,
+									this.columnHeader5});
+			this.listView1.Location = new System.Drawing.Point(65, 419);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(1040, 249);
+			this.listView1.TabIndex = 38;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			// 
+			// columnHeader1
+			// 
+			this.columnHeader1.Text = "Producto";
+			this.columnHeader1.Width = 219;
+			// 
+			// columnHeader2
+			// 
+			this.columnHeader2.Text = "Precio";
+			this.columnHeader2.Width = 163;
+			// 
+			// columnHeader3
+			// 
+			this.columnHeader3.Text = "Cantidad";
+			this.columnHeader3.Width = 153;
+			// 
+			// columnHeader4
+			// 
+			this.columnHeader4.Text = "Total";
+			this.columnHeader4.Width = 160;
+			// 
+			// TxtTotal
+			// 
+			this.TxtTotal.Location = new System.Drawing.Point(394, 383);
+			this.TxtTotal.Name = "TxtTotal";
+			this.TxtTotal.Size = new System.Drawing.Size(100, 22);
+			this.TxtTotal.TabIndex = 37;
+			this.TxtTotal.TextChanged += new System.EventHandler(this.TxtTotalTextChanged);
+			// 
+			// TxtCantidad
+			// 
+			this.TxtCantidad.Location = new System.Drawing.Point(248, 383);
+			this.TxtCantidad.Name = "TxtCantidad";
+			this.TxtCantidad.Size = new System.Drawing.Size(100, 22);
+			this.TxtCantidad.TabIndex = 36;
+			// 
+			// TxtPrecio
+			// 
+			this.TxtPrecio.Location = new System.Drawing.Point(111, 383);
+			this.TxtPrecio.Name = "TxtPrecio";
+			this.TxtPrecio.Size = new System.Drawing.Size(100, 22);
+			this.TxtPrecio.TabIndex = 35;
+			// 
+			// LblCantidad
+			// 
+			this.LblCantidad.Location = new System.Drawing.Point(248, 330);
+			this.LblCantidad.Name = "LblCantidad";
+			this.LblCantidad.Size = new System.Drawing.Size(100, 23);
+			this.LblCantidad.TabIndex = 33;
+			this.LblCantidad.Text = "Cantidad";
+			this.LblCantidad.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// LblTotal
+			// 
+			this.LblTotal.Location = new System.Drawing.Point(394, 330);
+			this.LblTotal.Name = "LblTotal";
+			this.LblTotal.Size = new System.Drawing.Size(100, 23);
+			this.LblTotal.TabIndex = 34;
+			this.LblTotal.Text = "Total";
+			this.LblTotal.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// LblPrecio
+			// 
+			this.LblPrecio.Location = new System.Drawing.Point(98, 330);
+			this.LblPrecio.Name = "LblPrecio";
+			this.LblPrecio.Size = new System.Drawing.Size(100, 23);
+			this.LblPrecio.TabIndex = 32;
+			this.LblPrecio.Text = "Precio";
+			this.LblPrecio.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// cmbProducto
+			// 
+			this.cmbProducto.FormattingEnabled = true;
+			this.cmbProducto.Items.AddRange(new object[] {
+									"\"Pastel de Chocolate\",",
+									"\"Pastel de Vainilla\",",
+									"\"Pastel de Tres Leches\",",
+									"\"Cheesecake\",",
+									"\"Cupcake\",",
+									"\"Brownie\",",
+									"\"Pay de Limón\",",
+									"\"Gelatina\",",
+									"\"Flan Napolitano\",",
+									"\"Donas\"",
+									"",
+									"\"Empanada de Queso\",",
+									"\"Quiche de Jamón\",",
+									"\"Pan de Ajo\",",
+									"\"Croissant Salado\",",
+									"\"Pastel de Carne\",",
+									"\"Volován\",",
+									"\"Pizza Individual\",",
+									"\"Tarta Salada\",",
+									"\"Panini\",",
+									"\"Enchilada\""});
+			this.cmbProducto.Location = new System.Drawing.Point(473, 220);
+			this.cmbProducto.Name = "cmbProducto";
+			this.cmbProducto.Size = new System.Drawing.Size(162, 24);
+			this.cmbProducto.TabIndex = 31;
+			// 
+			// LblProducto
+			// 
+			this.LblProducto.Location = new System.Drawing.Point(261, 220);
+			this.LblProducto.Name = "LblProducto";
+			this.LblProducto.Size = new System.Drawing.Size(172, 23);
+			this.LblProducto.TabIndex = 30;
+			this.LblProducto.Text = "Seleccione Producto";
+			// 
+			// RbdSalado
+			// 
+			this.RbdSalado.Location = new System.Drawing.Point(84, 267);
+			this.RbdSalado.Name = "RbdSalado";
+			this.RbdSalado.Size = new System.Drawing.Size(104, 24);
+			this.RbdSalado.TabIndex = 29;
+			this.RbdSalado.TabStop = true;
+			this.RbdSalado.Text = "Salado";
+			this.RbdSalado.UseVisualStyleBackColor = true;
+			// 
+			// RbdDulce
+			// 
+			this.RbdDulce.Location = new System.Drawing.Point(84, 228);
+			this.RbdDulce.Name = "RbdDulce";
+			this.RbdDulce.Size = new System.Drawing.Size(104, 24);
+			this.RbdDulce.TabIndex = 28;
+			this.RbdDulce.TabStop = true;
+			this.RbdDulce.Text = "Dulce";
+			this.RbdDulce.UseVisualStyleBackColor = true;
+			// 
+			// BtnLimpiar
+			// 
+			this.BtnLimpiar.Location = new System.Drawing.Point(587, 268);
+			this.BtnLimpiar.Name = "BtnLimpiar";
+			this.BtnLimpiar.Size = new System.Drawing.Size(223, 32);
+			this.BtnLimpiar.TabIndex = 54;
+			this.BtnLimpiar.Text = "Limpiar";
+			this.BtnLimpiar.UseVisualStyleBackColor = true;
+			this.BtnLimpiar.Click += new System.EventHandler(this.BtnLimpiarClick);
 			// 
 			// Postres
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.Color.FromArgb(255, 255, 192);
-			this.ClientSize = new System.Drawing.Size(334, 188);
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+			this.ClientSize = new System.Drawing.Size(1302, 1055);
+			this.Controls.Add(this.BtnLimpiar);
+			this.Controls.Add(this.TxtBorrar);
+			this.Controls.Add(this.BtnGuardar);
+			this.Controls.Add(this.TxtCambio);
+			this.Controls.Add(this.LblCambio);
+			this.Controls.Add(this.TxtImportePagado);
+			this.Controls.Add(this.TxtImporteNeto);
+			this.Controls.Add(this.LblImportePagado);
+			this.Controls.Add(this.LblImporteNeto);
+			this.Controls.Add(this.TxtDescuento);
+			this.Controls.Add(this.LblDescuento);
+			this.Controls.Add(this.TxtSubtotal);
+			this.Controls.Add(this.LblSubtotal);
+			this.Controls.Add(this.BtnEliminarProducto);
+			this.Controls.Add(this.BtnAgregarProducto);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.BtnImprimir);
+			this.Controls.Add(this.listView1);
+			this.Controls.Add(this.TxtTotal);
+			this.Controls.Add(this.TxtCantidad);
+			this.Controls.Add(this.TxtPrecio);
+			this.Controls.Add(this.LblCantidad);
+			this.Controls.Add(this.LblTotal);
+			this.Controls.Add(this.LblPrecio);
+			this.Controls.Add(this.cmbProducto);
+			this.Controls.Add(this.LblProducto);
+			this.Controls.Add(this.RbdSalado);
+			this.Controls.Add(this.RbdDulce);
 			this.Controls.Add(this.btnRegresar);
 			this.Name = "Postres";
 			this.Text = "Postres";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 		}
+		private System.Windows.Forms.Button BtnLimpiar;
+		private System.Windows.Forms.RadioButton RbdDulce;
+		private System.Windows.Forms.RadioButton RbdSalado;
+		private System.Windows.Forms.Label LblProducto;
+		private System.Windows.Forms.ComboBox cmbProducto;
+		private System.Windows.Forms.Label LblPrecio;
+		private System.Windows.Forms.Label LblTotal;
+		private System.Windows.Forms.Label LblCantidad;
+		private System.Windows.Forms.TextBox TxtPrecio;
+		private System.Windows.Forms.TextBox TxtCantidad;
+		private System.Windows.Forms.TextBox TxtTotal;
+		private System.Windows.Forms.ColumnHeader columnHeader4;
+		private System.Windows.Forms.ColumnHeader columnHeader3;
+		private System.Windows.Forms.ColumnHeader columnHeader2;
+		private System.Windows.Forms.ColumnHeader columnHeader1;
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.Button BtnImprimir;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button BtnAgregarProducto;
+		private System.Windows.Forms.Button BtnEliminarProducto;
+		private System.Windows.Forms.Label LblSubtotal;
+		private System.Windows.Forms.TextBox TxtSubtotal;
+		private System.Windows.Forms.Label LblDescuento;
+		private System.Windows.Forms.TextBox TxtDescuento;
+		private System.Windows.Forms.ColumnHeader columnHeader5;
+		private System.Windows.Forms.Label LblImporteNeto;
+		private System.Windows.Forms.Label LblImportePagado;
+		private System.Windows.Forms.TextBox TxtImporteNeto;
+		private System.Windows.Forms.TextBox TxtImportePagado;
+		private System.Windows.Forms.Label LblCambio;
+		private System.Windows.Forms.TextBox TxtCambio;
+		private System.Windows.Forms.Button BtnGuardar;
+		private System.Windows.Forms.Button TxtBorrar;
 
 		private System.Windows.Forms.Button btnRegresar;
+		
+		
 	}
 }
--- login/Postres.cs
+++ login/Postres.cs
@@ -6,9 +6,12 @@
  * 
  * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
  */
+
 using System;
 using System.Drawing;
+using System.IO;
 using System.Windows.Forms;
+using System.Drawing.Printing;
 
 namespace login
 {
@@ -17,131 +20,226 @@ namespace login
 		string[] productosDulces = {
 			"Pastel de Chocolate",
 			"Pastel de Vainilla",
-			"Pastel de Tres Leches",
 			"Cheesecake",
 			"Cupcake",
-			"Brownie",
-			"Pay de Limón",
-			"Gelatina",
-			"Flan Napolitano",
-			"Donas"
+			"Brownie"
 		};
 
 		string[] productosSalados = {
 			"Empanada de Queso",
-			"Quiche de Jamón",
-			"Pan de Ajo",
-			"Croissant Salado",
-			"Pastel de Carne",
-			"Volován",
 			"Pizza Individual",
-			"Tarta Salada",
 			"Panini",
-			"Enchilada"
+			"Quiche",
+			"Pan de Ajo"
 		};
 
 		public Postres()
 		{
 			InitializeComponent();
 			this.StartPosition = FormStartPosition.CenterScreen;
-			this.Size = new Size(1200, 700);
 
-			rbdDulce.CheckedChanged += RbdDulceCheckedChanged;
-			rbdSalado.CheckedChanged += RbdSaladoCheckedChanged;
+			RbdDulce.CheckedChanged += RbdDulceCheckedChanged;
+			RbdSalado.CheckedChanged += RbdSaladoCheckedChanged;
+
+			cmbProducto.SelectedIndexChanged += cmbProductoSelectedIndexChanged;
+
+			TxtPrecio.KeyPress += SoloNumeros;
+			TxtCantidad.KeyPress += SoloNumeros;
+			TxtImportePagado.KeyPress += SoloNumeros;
+			TxtDescuento.KeyPress += SoloNumeros;
 		}
 
-		void BtnRegresarClick(object sender, EventArgs e)
+		void RbdDulceCheckedChanged(object sender, EventArgs e)
 		{
-			Principal formularioPrincipal = new Principal();
-			formularioPrincipal.Show();
-			this.Hide();
+			if (RbdDulce.Checked)
+			{
+				cmbProducto.Items.Clear();
+				foreach (string p in productosDulces)
+					cmbProducto.Items.Add(p);
+				cmbProducto.SelectedIndex = 0;
+			}
+		}
+
+		void RbdSaladoCheckedChanged(object sender, EventArgs e)
+		{
+			if (RbdSalado.Checked)
+			{
+				cmbProducto.Items.Clear();
+				foreach (string p in productosSalados)
+					cmbProducto.Items.Add(p);
+				cmbProducto.SelectedIndex = 0;
+			}
+		}
+
+		void cmbProductoSelectedIndexChanged(object sender, EventArgs e)
+		{
+			switch (cmbProducto.Text)
+			{
+				case "Pastel de Chocolate": TxtPrecio.Text = "250"; break;
+				case "Pastel de Vainilla": TxtPrecio.Text = "220"; break;
+				case "Cheesecake": TxtPrecio.Text = "300"; break;
+				case "Cupcake": TxtPrecio.Text = "40"; break;
+				case "Brownie": TxtPrecio.Text = "50"; break;
+
+				case "Empanada de Queso": TxtPrecio.Text = "35"; break;
+				case "Pizza Individual": TxtPrecio.Text = "80"; break;
+				case "Panini": TxtPrecio.Text = "70"; break;
+				case "Quiche": TxtPrecio.Text = "90"; break;
+				case "Pan de Ajo": TxtPrecio.Text = "30"; break;
+			}
+		}
+
+		void SoloNumeros(object sender, KeyPressEventArgs e)
+		{
+			if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != '.')
+				e.Handled = true;
 		}
 
 		void BtnAgregarProductoClick(object sender, EventArgs e)
 		{
 			if (TxtPrecio.Text == "" || TxtCantidad.Text == "")
-			{
-				MessageBox.Show("Ingrese precio y cantidad");
-				return;
-			}
+	{
+		MessageBox.Show("Ingrese precio y cantidad");
+		return;
+	}
 
-			if (!rbdDulce.Checked && !rbdSalado.Checked)
-			{
-				MessageBox.Show("Seleccione si es Dulce o Salado");
-				return;
-			}
+	decimal precio = Convert.ToDecimal(TxtPrecio.Text);
+	decimal cantidad = Convert.ToDecimal(TxtCantidad.Text);
 
-			decimal precio, cantidad;
-			if (!decimal.TryParse(TxtPrecio.Text, out precio)) return;
-			if (!decimal.TryParse(TxtCantidad.Text, out cantidad)) return;
+	// TOTAL DEL PRODUCTO (SIN DESCUENTO)
+	decimal total = precio * cantidad;
+	TxtTotal.Text = total.ToString("0.00");
 
-			string tipo = rbdDulce.Checked ? "Dulce" : "Salado";
+	string tipo = RbdDulce.Checked ? "Dulce" : "Salado";
 
-			decimal totalProducto = precio * cantidad;
-			TxtTotal.Text = totalProducto.ToString("0.00");
+	ListViewItem fila = new ListViewItem(cmbProducto.Text);
+	fila.SubItems.Add(precio.ToString("0.00"));
+	fila.SubItems.Add(cantidad.ToString());
+	fila.SubItems.Add(total.ToString("0.00"));
+	fila.SubItems.Add(tipo);
 
-			ListViewItem fila = new ListViewItem(cmbProducto.Text);
-			fila.SubItems.Add(precio.ToString("0.00"));
-			fila.SubItems.Add(cantidad.ToString());
-			fila.SubItems.Add(totalProducto.ToString("0.00"));
-			fila.SubItems.Add(tipo);
-			listView1.Items.Add(fila);
+	listView1.Items.Add(fila);
 
-			RecalcularTotales();
+	RecalcularTotales();
 		}
-
-		void TxtImportePagadoTextChanged(object sender, EventArgs e)
-		{
-			RecalcularCambio();
-		}
-
-		void BtnEliminarProductoClick(object sender, EventArgs e)
-		{
-			if (listView1.SelectedItems.Count == 0)
-			{
-				MessageBox.Show("Seleccione un producto para eliminar");
-				return;
-			}
-
-			listView1.Items.Remove(listView1.SelectedItems[0]);
-			RecalcularTotales();
-		}
-
-		// ================= MÉTODOS DE APOYO =================
 
 		void RecalcularTotales()
 		{
 			decimal subtotal = 0m;
+
 			foreach (ListViewItem item in listView1.Items)
-			{
 				subtotal += Convert.ToDecimal(item.SubItems[3].Text);
-			}
+
 			TxtSubtotal.Text = subtotal.ToString("0.00");
 
 			decimal descuento = 0m;
 			if (TxtDescuento.Text != "")
-			{
-				decimal porc;
-				if (decimal.TryParse(TxtDescuento.Text, out porc))
-					descuento = subtotal * (porc / 100m);
-			}
+				descuento = subtotal * (Convert.ToDecimal(TxtDescuento.Text) / 100m);
 
-			decimal importeNeto = subtotal - descuento;
-			TxtImporteNeto.Text = importeNeto.ToString("0.00");
+			decimal neto = subtotal - descuento;
+
+			TxtImporteNeto.Text = neto.ToString("0.00");
+
+
+			TxtTotal.Text = neto.ToString("0.00");
 
 			RecalcularCambio();
 		}
 
 		void RecalcularCambio()
 		{
-			decimal pagado, neto;
-			if (!decimal.TryParse(TxtImportePagado.Text, out pagado)) return;
-			if (!decimal.TryParse(TxtImporteNeto.Text, out neto)) return;
+			if (TxtImportePagado.Text == "") return;
 
-			decimal cambio = pagado - neto;
+			decimal cambio = Convert.ToDecimal(TxtImportePagado.Text) -
+			                 Convert.ToDecimal(TxtImporteNeto.Text);
+
 			if (cambio < 0) cambio = 0;
-
 			TxtCambio.Text = cambio.ToString("0.00");
+		}
+
+		void BtnGuardarClick(object sender, EventArgs e)
+		{
+			StreamWriter sw = new StreamWriter("ticket.txt");
+
+			sw.WriteLine("=== TICKET ===");
+			foreach (ListViewItem item in listView1.Items)
+				sw.WriteLine(item.Text + " - $" + item.SubItems[3].Text);
+
+			sw.WriteLine("Total: $" + TxtTotal.Text);
+			sw.Close();
+
+			MessageBox.Show("Ticket guardado correctamente");
+		}
+
+		void BtnImprimirClick(object sender, EventArgs e)
+		{
+			PrintDocument doc = new PrintDocument();
+			doc.PrintPage += Imprimir;
+			doc.Print();
+		}
+
+		void Imprimir(object sender, PrintPageEventArgs e)
+		{
+			float y = 20;
+			e.Graphics.DrawString("TICKET", new Font("Arial", 12), Brushes.Black, 20, y);
+			y += 30;
+
+			foreach (ListViewItem item in listView1.Items)
+			{
+				e.Graphics.DrawString(
+					item.Text + " $" + item.SubItems[3].Text,
+					new Font("Arial", 10),
+					Brushes.Black,
+					20, y);
+				y += 20;
+			}
+
+			y += 20;
+			e.Graphics.DrawString(
+				"Total: $" + TxtTotal.Text,
+				new Font("Arial", 11, FontStyle.Bold),
+				Brushes.Black, 20, y);
+		}
+		
+		void TxtTotalTextChanged(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void BtnLimpiarClick(object sender, EventArgs e)
+		{
+
+	TxtPrecio.Text = "";
+	TxtCantidad.Text = "";
+	TxtTotal.Text = "";
+	TxtSubtotal.Text = "";
+	TxtDescuento.Text = "";
+	TxtImporteNeto.Text = "";
+	TxtImportePagado.Text = "";
+	TxtCambio.Text = "";
+	TxtTotal.Text = "";
+
+
+	cmbProducto.Items.Clear();
+	cmbProducto.Text = "";
+
+
+	RbdDulce.Checked = false;
+	RbdSalado.Checked = false;
+
+
+
+		}
+		void BtnEliminarProductoClick(object sender, System.EventArgs e)
+		{
+			if (listView1.SelectedItems.Count == 0)
+	{
+		MessageBox.Show("Seleccione un producto para eliminar");
+		return;
+	}
+
+	listView1.Items.Remove(listView1.SelectedItems[0]);
+	RecalcularTotales();
 		}
 	}
 }
